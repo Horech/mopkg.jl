c112215c8d8c100cf8156138314107a8eea74020
PioFry <piotrfrycki@gmail.com>
2020-05-10 13:20:34 +0200

    Added example opt. func.

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -1,5 +1,39 @@
 module mopkg
 
-greet() = print("Hello World!")
+"Divide by half method implementation finds local minimum for single variable functions"
+function divByHalf(f, a, b, err = 1e-5, maxiter = 10000)
+	middle = (a+b)/2
+	diff = b - a
+	iter= 0
+
+	if(a >= b)
+		print("a parameter must be smaller than b parameter")
+		return
+	end
+
+	while(abs(diff) >= err)
+		iter += 1
+		left = a + diff/4
+		right = b - diff/4
+		if(f(left) < f(middle))
+			b = middle
+			middle = left
+		elseif(f(right) < f(middle))
+			a = middle
+			middle = right
+		else
+			a = left
+			b = right
+		end
+		diff = b - a
+		if(iter > maxiter)
+			print("Max iterations exceeded")
+			return
+		end
+	end
+	return (middle, iter) end
+
+	export divByHalf
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using mopkg
 using Test
 
 @testset "mopkg.jl" begin
-    # Write your own tests here.
+    @test divByHalf(x -> x^3 * x - sin(x), -100, 200) == (0.5919843912124634, 25)
+
+    @test divByHalf(x -> x^3 * x^2 * x * exp(2) - sin(x), -100, 200) == (0.4583433270454407, 25)
 end
