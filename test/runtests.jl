ab283f37fdb90c7845bd18f81ffe62887af2f8e1
PioFry <piotrfrycki@gmail.com>
2020-05-16 12:19:41 +0200

    adjusted test result-check order to previously changed return order

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using mopkg
 using Test
 
 @testset "mopkg.jl" begin
-    @test divByHalf(x -> x^3 * x - sin(x), -100) == (0.5919828545302153, -0.43519677568752524)
+    @test divByHalf(x -> x^3 * x - sin(x), -100) == (-0.43519677568752524, 0.5919828545302153)
 
-    @test divByHalf(x -> x^3 * x^2 * x * exp(2) - sin(x), -100) == (0.4583420231938362, -0.3739561126977562)
+    @test divByHalf(x -> x^3 * x^2 * x * exp(2) - sin(x), -100) == (-0.3739561126977562, 0.4583420231938362)
 end
